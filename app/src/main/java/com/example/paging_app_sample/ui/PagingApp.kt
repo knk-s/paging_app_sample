1520613220164e2e43832c533f958fb158cdd527
knk-s <jm.mdngt.cre@gmail.com>
2025-02-16 21:38:15 +0900

    AppBarを追加

--- app/src/main/java/com/example/paging_app_sample/ui/PagingApp.kt
+++ app/src/main/java/com/example/paging_app_sample/ui/PagingApp.kt
@@ -2,15 +2,30 @@ package com.example.paging_app_sample.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.paging_app_sample.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PagingApp() {
-    Scaffold { innerPadding ->
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        topBar = {
+            AppBar(scrollBehavior)
+        }
+    ) { innerPadding ->
         Surface(
             modifier = Modifier
                 .fillMaxSize()
@@ -20,4 +35,19 @@ fun PagingApp() {
             RecipesScreen(viewModel = recipesViewModel)
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_title),
+                style = MaterialTheme.typography.titleLarge
+            )
+        },
+        scrollBehavior = scrollBehavior,
+        modifier = modifier
+    )
 }
